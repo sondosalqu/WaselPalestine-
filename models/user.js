22a3ff136558258b4f375d22b7b9da6af970ad61
MaiarHadi <mayarobaid850@gmail.com>
2026-03-13 22:00:11 +0200

    resolve merge

--- models/user.js
+++ models/user.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use strict';
 
 module.exports = (sequelize, DataTypes) => {
@@ -39,53 +38,9 @@ module.exports = (sequelize, DataTypes) => {
     },
     {
       tableName: "users",
-      timestamps: false, 
-    }
-  );
-
-  return User;
-=======
-module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define(
-    "User",
-    {
-      user_id: {
-        type: DataTypes.BIGINT.UNSIGNED,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      name: {
-        type: DataTypes.STRING(100),
-        allowNull: false,
-      },
-      email: {
-        type: DataTypes.STRING(150),
-        allowNull: false,
-        unique: true,
-      },
-      password: {
-        type: DataTypes.STRING(255),
-        allowNull: false,
-      },
-      phone_number: {
-        type: DataTypes.STRING(30),
-        allowNull: true,
-      },
-      role_id: {
-        type: DataTypes.INTEGER.UNSIGNED,
-        allowNull: false,
-      },
-      created_at: {
-        type: DataTypes.DATE,
-        allowNull: false,
-      },
-    },
-    {
-      tableName: "users",
       timestamps: false,
     }
   );
 
   return User;
->>>>>>> e230d9f (feat: add checkpoint and incident endpoints, update incident model, and enhance signin validation)
 };
